2268fc469e91b8b8e556a1ba315b37c0229c11aa
Maria Jose Mendez <majomendez88mena@gmail.com>
2020-10-05 17:37:27 -0600

    feat(form): form structure created
    form structure for search recipes structure

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { Fragment } from "react";
 
 const Form = () => {
-  return ( 
-    <h1>Form</h1>
-   );
-}
- 
+  return (
+    <Fragment>
+      <form className="col-12">
+        <fieldset className="text-center">
+          <legend>Search drinks by categories or ingredients</legend>
+        </fieldset>
+        <div className="row mt-10">
+          <div className="col-md-4">
+            <input
+              name="name"
+              className="form-control"
+              type="text"
+              placeholder="Search by ingredient"
+            />
+          </div>
+          <div className="col-md-4">
+            <select className="form-control" name="category"></select>
+          </div>
+          <div className="col-md-4">
+            <input
+              name="name"
+              className="btn btn-block btn-primary"
+              type="submit"
+              value="Search"
+            />
+          </div>
+        </div>
+      </form>
+    </Fragment>
+  );
+};
+
 export default Form;
+
+/**
+ * <form className="col-12">
+			<fieldset className="text-center">
+				<legend>Search drinks by categories or ingredients</legend>
+				<div className="row">
+					<div className="col-md-4">
+						<input
+							name="name"
+							className="form-control"
+							type="text"
+							placeholder="Search by ingredient"
+						/>
+					</div>
+				</div>
+			</fieldset>
+		</form>
+ */
